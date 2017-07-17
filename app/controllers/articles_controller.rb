ac65270e26b946a56a096be436615f9bd5589d6b
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-17 12:33:09 +0200

    add flash for updating the Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,9 @@ class ArticlesController < ApplicationController
 	def update 	# 6
 		@article = Article.find(params[:id])
 		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated!"
+		
 		redirect_to article_path(@article)
 	end
 
