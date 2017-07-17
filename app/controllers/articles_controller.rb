e111bf2bfddbd9abf43e3c4d984247c6687ff235
Salomanuel <salomanuel.aux+code@gmail.com>
2017-07-17 12:05:03 +0200

    add articles edit and a partial for forms

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,23 +1,33 @@
 class ArticlesController < ApplicationController
-	def index
+	def index		# 1
 		@articles = Article.all
 	end
 
-	def show
+	def show		# 2
 		@article = Article.find(params[:id])
 	end
 
-	def new
+	def new 		# 3
 		@article = Article.new
 	end
 
-	def create
+	def create	# 4
 		@article = Article.new(article_params)
 		@article.save
 		redirect_to @article
 	end
 
-	def destroy
+	def edit		# 5
+		@article = Article.find(params[:id])
+	end
+
+	def update 	# 6
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+		redirect_to article_path(@article)
+	end
+
+	def destroy	# 7
 		@article = Article.find(params[:id])
 		@article.destroy
 		redirect_to articles_path
